a722a29bb07d01cba50c7dd8a674f4fefb7a48fe
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-05-30 16:32:08 -0400

    Add new toy, and like button

--- src/index.js
+++ src/index.js
@@ -27,23 +27,74 @@ function getToys() {
 function buildCard(data) {
   const toyCollection = document.getElementById('toy-collection');
   for (let toy in data) {
+    //create Div for our card.
     const cardDiv = document.createElement( 'div' )
     cardDiv.className = 'card'
+    //Create h2 element and fill it
     const nameHeading = document.createElement( 'h2' )
-    nameHeading.innerText = data[toy].name 
+    nameHeading.innerText = data[toy].name
+    //create image element and fill it 
     const img = document.createElement( 'img' )
     img.src = data[toy].image
     img.className = ' toy-avatar '
+    img.alt = data[toy].name
+    //create p element and fill it
     const pElement = document.createElement( 'p' )
     pElement.innerText = `${data[toy].likes} likes`
+    //create button element and fill it
     const button = document.createElement( 'button' )
     button.className = "like-btn"
     button.id = "[toy_id]"
     button.innerText = 'Like ❤️'
+    //appending all our elements to the card Div
     cardDiv.append( nameHeading, img, pElement, button )
+    //appending our car Div to the Toy Collection Div 
     toyCollection.append( cardDiv )
 
-    console.log(cardDiv)
+    //Like Button Functionality
+    button.addEventListener('click', (e) => {
+      data[toy].likes++
+      pElement.innerText = `${data[toy].likes} likes`
+    })
+    //console.log(cardDiv)
     //console.log(data[toy].name) 
   }
+}
+
+// Handling the Submit Button and Creating New Card.
+document.querySelector('form').addEventListener('submit', handleSubmit);
+
+let idNumber = 9;
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  let newCard = [{
+    id: idNumber,
+    name: e.target.name.value,
+    image: e.target.image.value,
+    likes: 0
+  }]
+  idNumber++;
+  //console.log(newCard)
+  buildCard(newCard);
+  postCard(newCard[0]);
+  //console.log(e.target.image.value);
+}
+
+
+
+
+function postCard(newCard) {
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body:JSON.stringify(newCard)
+  })
+  .then(res => res.json())
+  .then(card => console.log(card))  
 }
